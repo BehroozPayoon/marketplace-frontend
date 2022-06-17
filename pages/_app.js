c8815bfdd70c4c734a23c19a1ffc394e44b70326
Behrooz Payoon <Behrooz.payoon@gmail.com>
2022-06-17 15:18:52 +0430

    Add web3uikit and header

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { MoralisProvider } from "react-moralis";
+import Head from "next/head";
+import Header from "../components/Header";
+import { NotificationProvider } from "web3uikit";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>NFT Marketplace</title>
+        <meta name="description" content="NFT Marketplace" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <NotificationProvider>
+          <Header />
+          <Component {...pageProps} />
+        </NotificationProvider>
+      </MoralisProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
